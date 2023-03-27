a242af92a5e7dcd6629b495e810716b91d2431f0
duyetDz <90817657+duyetDz@users.noreply.github.com>
2023-03-27 14:11:30 +0700

    edit_database_user_Xư_lyLogin-reg_user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('fullname')->nullable();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->string('adress')->nullable();
             $table->string('password');
